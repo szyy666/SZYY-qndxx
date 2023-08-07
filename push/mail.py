10142f17189d73917ef4aa454966c8b42562944f
Chenghow <45912075+Chenghow@users.noreply.github.com>
2023-08-07 13:14:59 +0800

    🐞 fix: 修复email发送

--- push/mail.py
+++ push/mail.py
@@ -4,10 +4,10 @@ from email.header import Header
 def push(title,htmlcontent,config):
     sender=config['sender']
     passwd=config['password']
-    receiver=[config['receiver']]
+    receiver=config['receiver']
     message = MIMEText(htmlcontent, 'html', 'utf-8')
-    message['From'] = sender
-    message['To'] = receiver
+    message['From'] = Header("爱你的团团", 'utf-8')
+    message['To'] =  Header("社会主义接班人", 'utf-8')
     message['Subject'] = Header(title, 'utf-8')
     try:
         smtp = smtplib.SMTP()
